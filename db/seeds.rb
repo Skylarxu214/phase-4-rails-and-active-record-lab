efe60e6c153f32ee5035078ceb7e3cd30a9c25c5
Tian Xu <xueqingxu0302@gmail.com>
2022-01-31 22:50:29 -0700

    commit tree b0f8c99cc9b60bf371c33d0a486dc00343e041f6

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Student.create!(first_name: "Tian", last_name: "X", grade: "7" )
